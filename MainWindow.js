13aceff2b264acc2eef529d2da6ae6c40e15dec8
Bytedefined <kniazievyaroslav@gmail.com>
2021-01-29 08:02:45 +0800

    Update MainWindow.js

--- MainWindow.js
+++ MainWindow.js
@@ -3,7 +3,7 @@ const { BrowserWindow } = require('electron')
 class MainWindow extends BrowserWindow {
   constructor(file, isDev) {
     super({
-      title: 'Bytetrex CPU Monitor',
+      title: 'Bytedefined\'s CPU Usage Monitor',
       width: isDev ? 800 : 355,
       height: 500,
       icon: `${__dirname}/assets/icons/icon.png`,
